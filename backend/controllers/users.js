215089e48cd5c3769ca09ec66448371d7db77373
mmgonnar <gn.mariela@gmail.com>
2025-03-25 11:15:19 -0600

    Updated users controller

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,131 +1,115 @@
 const bcrypt = require("bcryptjs");
 const User = require("../models/user");
 const Card = require("../models/card");
-const { deleteCard } = require("./cards");
 
-const getUsers = async (req, res) => {
+const getUsers = async (req, res, next) => {
   try {
     const users = await User.find().orFail(new Error("document not found"));
     res.json(users);
   } catch (err) {
-    res.status(500).json({ message: err.message });
+    req.type = "user";
+    next(err);
   }
 };
 
-const getUserById = async (req, res) => {
+const getUserById = async (req, res, next) => {
   try {
     const user = await User.findById(req.user._id).orFail(
       new Error("No user with that id has been found.")
     );
     if (!user) {
-      return res.status(404).json({ message: "User not found" });
+      const error = new Error("");
+      error.status = 404;
+      throw error;
     }
     res.json(user);
   } catch (err) {
-    res.status(500).json({ message: err.message });
+    req.type = "user";
+    next(err);
   }
 };
 
-const createUser = async (req, res) => {
-  const { email, password, name, about, avatar } = req.body;
-  bcrypt
-    .hash(password, 10)
-    .then((hash) => {
-      const newUser = new User({
-        email,
-        password: hash,
-        name,
-        about,
-        avatar,
-      });
+const createUser = async (req, res, next) => {
+  try {
+    const { email, password, name, about, avatar } = req.body;
+    const hash = await bcrypt.hash(password, 10);
 
-      return newUser;
-    })
-    .then(async (newUser) => {
-      const savedUser = await newUser.save();
-      //const defaultUser = new
-      const defaultCard = new Card({
-        name: "Welcome!",
-        link: "https://www.pushengage.com/wp-content/uploads/2022/02/Best-Website-Welcome-Message-Examples.png",
-        owner: savedUser._id,
-      });
-
-      await defaultCard.save();
-      res.status(201).json(savedUser);
-    })
-    .catch((err) => {
-      res.status(500).json({ message: err.message });
+    const newUser = new User({
+      email,
+      password: hash,
+      name,
+      about,
+      avatar,
     });
+
+    const savedUser = await newUser.save();
+
+    const defaultCard = new Card({
+      name: "Welcome!",
+      link: "https://www.pushengage.com/wp-content/uploads/2022/02/Best-Website-Welcome-Message-Examples.png",
+      owner: savedUser._id,
+    });
+
+    await defaultCard.save();
+    res.status(201).json(savedUser);
+  } catch (err) {
+    req.type = "user";
+    next(err);
+  }
 };
 
 const updateUser = async (req, res, next) => {
-  const { name, about } = req.body;
   try {
+    const { name, about } = req.body;
     const updateUser = await User.findByIdAndUpdate(
       req.user._id,
-      {
-        name,
-        about,
-      },
+      { name, about },
       { new: true }
     );
+
     if (!updateUser) {
-      return next({ status: 400, message: "User not found" });
+      const error = new Error("");
+      error.status = 404;
+      throw error;
     }
     res.json(updateUser);
   } catch (err) {
-    if (err.message === "User not found") {
-      return res.status(404).json({ message: err.message });
-    }
-    res.status(500).json({ message: err.message });
+    req.type = "user";
+    next(err);
   }
-  // const { name, about } = req.body;
-  // try {
-  //   const updateUser = await User.findByIdAndUpdate(
-  //     req.user._id,
-  //     {
-  //       name,
-  //       about,
-  //     },
-  //     { new: true }
-  //   ).orFail(new Error("User not found"));
-  //   res.json(updateUser);
-  // } catch (err) {
-  //   if (err.message === "User not found") {
-  //     return res.status(404).json({ message: err.message });
-  //   }
-  //   res.status(500).json({ message: err.message });
-  // }
 };
 
-const updateAvatar = async (req, res) => {
-  const { avatar } = req.body;
+const updateAvatar = async (req, res, next) => {
   try {
-    const updateAvatar = await User.findByIdAndUpdate(req.user._id, {
-      avatar,
-    }).orFail(new Error("Document not found)"));
+    const { avatar } = req.body;
+    const updateAvatar = await User.findByIdAndUpdate(
+      req.user._id,
+      { avatar },
+      { new: true }
+    ).orFail(new Error("Document not found"));
+
     res.json(updateAvatar);
   } catch (err) {
-    if (err.message === "User not found") {
-      return res.status(404).json({ message: err.message });
-    }
-    res.status(500).json({ message: err.message });
+    req.type = "user";
+    next(err);
   }
 };
 
-const deleteUser = async (req, res) => {
-  const userId = req.user._id;
-  //deleteCard(param)
+const deleteUser = async (req, res, next) => {
   try {
+    const userId = req.user._id;
     const deletedUser = await User.findByIdAndDelete(userId);
 
     if (!deletedUser) {
-      return res.status(400).json({ message: "Delete user failed " });
+      const error = new Error("");
+      error.status = 404;
+      throw error;
     }
 
     res.json({ message: "User info deleted successfully" });
   } catch (err) {
-    res.status(500).json({ message: err.message });
+    req.type = "user";
+    next(err);
   }
 };
 
