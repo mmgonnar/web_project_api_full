d74c6d25972476710370e8f7eaafd470ffdb24c8
mmgonnar <gn.mariela@gmail.com>
2025-08-15 11:56:03 -0600

    Clean up CORS configuration - remove old backend URL

--- backend/middleware/cors.js
+++ backend/middleware/cors.js
@@ -1,12 +1,17 @@
 const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:3001",
+  "http://localhost:5173",
   "https://sudoa.crabdance.com",
   "https://api.sudoa.crabdance.com",
   "https://www.sudoa.crabdance.com",
-  "http://localhost:5173",
   "https://18-web-project-api-full-frontend.vercel.app",
   "https://18-web-project-api-full-backend.vercel.app",
+  "https://18-web-project-api-full-frontend-git-main-mariela-gonzalez-2024.vercel.app",
+  "https://18-web-project-api-full-backend-git-main-mariela-gonzalez-2024.vercel.app",
+  "https://18-web-project-api-full-backend-my4xqvqbh.vercel.app",
+  "https://18-web-project-api-full-backend-fr2fd75gp.vercel.app",
+  "https://18-web-project-api-full-frontend-jokvrys8s.vercel.app",
 ];
 
 const corsSettings = {
