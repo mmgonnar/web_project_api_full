832e3c2c341ebc35e31ce7897eae63d194a93be7
mmgonnar <gn.mariela@gmail.com>
2025-08-15 14:52:15 -0600

    Fixed urls

--- backend/middleware/cors.js
+++ backend/middleware/cors.js
@@ -5,18 +5,19 @@ const allowedOrigins = [
   "https://sudoa.crabdance.com",
   "https://api.sudoa.crabdance.com",
   "https://www.sudoa.crabdance.com",
-  "https://18-web-project-api-full-frontend.vercel.app",
-  "https://18-web-project-api-full-backend.vercel.app",
-  "https://18-web-project-api-full-frontend-git-main-mariela-gonzalez-2024.vercel.app",
-  "https://18-web-project-api-full-backend-git-main-mariela-gonzalez-2024.vercel.app",
-  "https://18-web-project-api-full-backend-my4xqvqbh.vercel.app",
-  "https://18-web-project-api-full-backend-fr2fd75gp.vercel.app",
-  "https://18-web-project-api-full-frontend-jokvrys8s.vercel.app",
 ];
 
 const corsSettings = {
   origin: (origin, callback) => {
     if (!origin) return callback(null, true);
+
+    if (
+      origin.includes("18-web-project-api-full-frontend") &&
+      origin.includes("vercel.app")
+    ) {
+      return callback(null, true);
+    }
+
     if (allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
